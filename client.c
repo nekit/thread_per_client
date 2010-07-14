885645c3d4870dc8e3599764cba2bc3c56d9d705
Nikita <nekitbgood@gmail.com>
2010-07-14 18:08:25 +0400

    client ready

--- client.c
+++ client.c
@@ -2,6 +2,8 @@
 #include "client_threads.h"
 #include <pthread.h>
 #include <memory.h>
+#include "logging.h"
+#include "client_threads.h"
 
 int make_client_task ( run_mode_t * rm, client_task_t * ct ) {
 
@@ -10,16 +12,36 @@ int make_client_task ( run_mode_t * rm, client_task_t * ct ) {
   ct -> port = rm -> port;
   memcpy( ct -> ip_addr, rm -> ip_addr, sizeof (rm -> ip_addr) );
 
+  ct -> statistic.counter = 0;
+  if ( 0 != pthread_mutex_init ( &ct -> statistic.mutex, NULL ) ) {
+
+    to_log ( "Init statistic mutex problem", LL_ERROR, rm -> log_level );
+    return 1;
+  }
+
   return 0;
 }
 
 int run_client ( run_mode_t run_mode ) {
 
-  /*pthread_t thread[run_mode.thread_amount];
+  pthread_t thread[run_mode.thread_amount];
   client_task_t client_task;
-  make_client_task ( &run_mode, &client_task );
-  */
+  if ( 0 != make_client_task ( &run_mode, &client_task ) ) {
 
+    to_log ( "make_client_task error", LL_ERROR, run_mode.log_level );
+    return 1;
+  }
+
+  int i;
+  for ( i = 0; i < run_mode.thread_amount; ++i )
+    if ( 0 != pthread_create( &thread[i], NULL, client, (void *) &client_task ) ) {
+
+      to_log ( "thread create problem", LL_ERROR, run_mode.log_level );
+      return 1;
+    }
+
+  for ( i = 0; i < run_mode.thread_amount; ++i )
+    pthread_join ( thread[i], NULL );  
 
   return 0;
 }
--- echo_client.c
+++ echo_client.c
@@ -3,13 +3,22 @@
 #include "structures.h"
 #include "parse_args.h"
 #include "client.h"
+#include "logging.h"
 
 int main ( int argc, char * argv[] ) {
-  run_mode_t rm;
-  if (1 == parse_args(argc, argv, &rm)){
+  
+  run_mode_t rm;  
+  if ( 0 != parse_args ( argc, argv, &rm ) ) {
     printf("parse argument exception");
     return (EXIT_FAILURE);
   }
+
+  if ( CM_CLIENT == rm.client_run_mode )
+    if ( 0 != run_client ( rm ) ) {
+
+      to_log ( "client error", LL_ERROR, rm.log_level );
+      return (EXIT_FAILURE);
+    }
   
   return (EXIT_SUCCESS);
 }
